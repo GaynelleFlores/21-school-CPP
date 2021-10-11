371c5ad56cb316f8aed05f6bff4e942bf0e1b90c
Gaynelle Flores <gflores@si-l2.kzn.21-school.ru>
2021-10-11 15:15:03 +0300

    first day

---
+++ Day01/ex02/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main ()
+{
+	std::string brain = "HI THIS IS BRAIN";
+	std::string *PTR = &brain;
+	std::string &REF = brain;
+
+	std::cout << "Address of the string:\n";
+	std::cout << &brain << std::endl;
+	std::cout << "Address of the string by using PTR:\n";
+	std::cout << PTR << std::endl;
+	std::cout << "Address of the string by using REF:\n";
+	std::cout << &REF << std::endl;
+
+	std::cout << "\nString:\n";
+	std::cout << brain << std::endl;
+	std::cout << "String by using PTR:\n";
+	std::cout << *PTR << std::endl;
+	std::cout << "String by using REF:\n";
+	std::cout << REF << std::endl;
+}
